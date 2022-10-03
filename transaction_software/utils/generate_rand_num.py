6f78ad76866560c67048919a89d387b709270f44
senthilnathan <libertariansenthil@gmail.com>
2022-10-03 18:58:41 +0530

    renamed acc_num.py

---
+++ transaction_software/utils/generate_rand_num.py
@@ -0,0 +1,18 @@
+import random
+import math
+
+def generate_account_number()->int:
+    """
+    Generates a unique 16 digit account number. The first 6 digits are prefixed number(i.e bank code) 113120. the remaining 10 digits are generated randomly. with that 36,28,800 account numbers can be created.
+    """
+    return random.randrange(start=0,stop=9999999999)
+
+def generate_trans_id()->int:
+    """
+    Return a random transaction number. with that 13_07_67_43_68_000 transaction id's can be created.
+    """
+    return random.randint(0, math.factorial(15))
+
+# drive code
+if __name__ == "__main__":
+    pass
