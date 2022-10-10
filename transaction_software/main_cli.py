7abe43571eaf9a6054773a66a49d798be0b5f5c2
Nithish <nitd020611@gmail.com>
2022-10-10 21:25:15 +0530

    minor addition

--- transaction_software/main_cli.py
+++ transaction_software/main_cli.py
@@ -79,7 +79,38 @@ def _display_main()->None:
                     time.sleep(3.0)
                     continue
             elif user_choice == 2:
-                pass
+                import mysql.connector
+
+                try:
+                    conn = mysql.connector.connect(host='localhost',
+                                                database='boi',
+                                                user='root',
+                                                password='password')
+
+                    conn.autocommit = False
+                    cursor = conn.cursor()
+                    # withdraw from account A 
+                    sql_update_query = """Update account_A set balance = 1000 where debit_account_number = 1131201234567890"""
+                    cursor.execute(sql_update_query)
+
+                    # Deposit to account B 
+                    sql_update_query = """Update account_B set balance = 1500 where debit_account_number = 1131201254567870"""
+                    cursor.execute(sql_update_query)
+                    print("Record Updated successfully ")
+
+                    # Commit your changes
+                    conn.commit()
+
+                except mysql.connector.Error as error:
+                    print("Failed to update record to database rollback: {}".format(error))
+                    # reverting changes because of exception
+                    conn.rollback()
+                finally:
+                    # closing database connection.
+                    if conn.is_connected():
+                        cursor.close()
+                        conn.close()
+                        print("connection is closed")
 
             # Update an Acccount Information.
             elif user_choice == 3:
